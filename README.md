e730b6650f7ad3f7fb3591b7e61b0ed4dfe5bceb
Raghav <codingkapur@gmail.com>
2021-10-22 15:51:20 +0530

    validate input and change background color to input

--- README.md
+++ README.md
@@ -10,3 +10,22 @@ Node version:
 2. Run command 'npm install"
 3. run command 'npm start'
 
+## Problems presented in this app
+1. How to validate input for a Hexadecimal color
+2. How to change border of input as validation takes place.
+3. How to programmatically change the intensity of a Hexadecimal color
+4. How to 
+
+
+Answer 1:
+regex = "^#([A-Fa-f0-9]{6})$";
+Where: 
+    ^ represents the starting of the string.
+    # represents the hexadecimal color code must start with a ‘#’ symbol.
+    ( represents the start of the group.
+    [A-Fa-f0-9]{6} represents the letter from a-f, A-F, or digit from 0-9 with a length of 6.
+    ) represents the end of the group.
+    $ represents the ending of the string.
+
+const regex = new RegExp('^#([A-Fa-f0-9]{6})$')
+regex.test(stringToBeTested)
--- src/App.js
+++ src/App.js
@@ -1,15 +1,58 @@
+import { useState } from "react";
 import Colors from "./components/Colors";
 
 function App() {
+  const [currentColor, setCurrentColor] = useState("");
+  const [input, setInput] = useState("");
+  const [inputError, setInputError] = useState();
+
+  //Create a regex express to test for valid Hex Color.
+  const regex = new RegExp("^#([A-Fa-f0-9]{6})$");
+
+  //Functions
+  const validateInput = (input) => {
+    setInput(input.toUpperCase().trim());
+    //Should start with a # symbol, should only contain digits between 0-9 and alphabets between a-f, should be 7 chars in total.
+    if (regex.test(input)) {
+      setInputError(false);
+    } else {
+      setInputError(true);
+    }
+  };
+  
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if(!inputError){
+      setCurrentColor(input)
+    }
+    else {
+      alert('That is not the right color')
+      setInput('#');
+      setCurrentColor('#')
+    }
+  };
+  //Inline styling variables
+  const borderColor = {
+    border: `1px solid #${inputError ? "ff0000" : "00ff00"}`,
+  };
+  const backgroundColor= {
+    backgroundColor: `${currentColor}`
+  }
+
+
+
   return (
-    <div className="app__container">
+    <div className="app__container" style={backgroundColor}>
       <h1 className="app__title">Color Palette</h1>
-      <div className="color__selector--container">
-        <form className="color__input--form">
+      <div className="color__selector--container" >
+        <form className="color__input--form " onSubmit={(e) => onSubmit(e)}>
           <input
             type="text"
+            value={input}
             placeholder="Enter Hex Value"
             className="color__input"
+            onChange={(e) => validateInput(e.target.value)}
+            style={borderColor}
           />
           <button className="btn submit-btn">Submit</button>
         </form>
